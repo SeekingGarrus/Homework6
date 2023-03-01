a5ae678358ed075d6e105b4e576eb3a7b1986338
@Olga.shka@ <shkarina.ol@yandex.ru>
2023-03-02 00:03:33 +0400

    Initial commit

---
+++ Task01/Program.cs
@@ -0,0 +1,43 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 1.Ввод числа
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+// 2. Объявление массива и ввод пользователем элементов массива
+int[] CreateArray(int size) // функция создания массива int
+{
+    return new int[size];
+}
+
+int[] FillArrayFor(int size) // метод по заполнению массива c for
+{
+    int[] array = CreateArray(size);
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Prompt($"Введите {i + 1} элемент массива: ");
+    }
+    return array;
+}
+
+// 3. Подсчет положительных чисел
+int CountEvenNumbers(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int size = Prompt("Введите размер массива: ");
+int[] array = FillArrayFor(size);
+Console.WriteLine($"Чисел больше нуля: {CountEvenNumbers(array)}");
---
+++ Task02/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+double GetIntValueFromUser(string text) // функция получения числа от пользователя в double
+{
+    double num = 0;
+    bool flag = true;
+    do
+    {
+        Console.Write($"{text}: ");
+        flag = Double.TryParse(Console.ReadLine(), out num);
+    } while (!flag);
+    return num;
+}
+
+// y = k1 * x + b1;
+// y = k2 * x + b2;
+// x = (b2 - b1) / (k1 - k2);
+// y = (k1 * (b2 - b1) / (k1 - k2)) + b1;
+
+string Formula(double b1, double b2, double k1, double k2)
+{
+    string result = " ";
+    double x = 0;
+    double y = 0;
+    x = (b2 - b1) / (k1 - k2);
+    y = (k1 * (b2 - b1) / (k1 - k2)) + b1;
+    result = $"Точка пересечения ({x}, {y})";
+    return result;
+}
+double b1 = GetIntValueFromUser("Введите значение 1: ");
+double b2 = GetIntValueFromUser("Введите значение 2: ");
+double k1 = GetIntValueFromUser("Введите значение 3: ");
+double k2 = GetIntValueFromUser("Введите значение 4: ");
+Console.WriteLine(Formula(b1, b2, k1, k2));
